6b97a510a3c28a73e306bf3e77e9b2e047ca031c
Mykola Kovalchuk <mikolakovalchuk1990@gmail.com>
2023-08-20 09:57:11 +0300

    керегування модалки

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,25 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
+
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    nameInput: document.getElementById('name'), // Додайте посилання на елемент вводу
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +27,15 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+
+    // Перевірте, чи зараз модальне вікно не приховане і чи існує елемент nameInput
+    if (!refs.modal.classList.contains('is-hidden') && refs.nameInput) {
+      refs.nameInput.focus();
+    }
   }
 })();
+
+
+
+
+
