887a8623601e23e92bdb845c3b13181d4f82af6e
Mykola Kovalchuk <mikolakovalchuk1990@gmail.com>
2023-08-30 21:29:14 +0300

    Підкоригував форму

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,8 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
-
-
 // (() => {
 //   const refs = {
 //     openModalBtn: document.querySelector('[data-modal-open]'),
 //     closeModalBtn: document.querySelector('[data-modal-close]'),
 //     modal: document.querySelector('[data-modal]'),
-//     nameInput: document.getElementById('name'), // Додайте посилання на елемент вводу
 //   };
 
 //   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -27,13 +10,30 @@
 
 //   function toggleModal() {
 //     refs.modal.classList.toggle('is-hidden');
-
-//     // Перевірте, чи зараз модальне вікно не приховане і чи існує елемент nameInput
-//     if (!refs.modal.classList.contains('is-hidden') && refs.nameInput) {
-//       refs.nameInput.focus();
-//     }
 //   }
 // })();
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    nameInput: document.getElementById('name'), // Додайте посилання на елемент вводу
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+
+    // Перевірте, чи зараз модальне вікно не приховане і чи існує елемент nameInput
+    if (!refs.modal.classList.contains('is-hidden') && refs.nameInput) {
+      refs.nameInput.focus();
+    }
+  }
+})();
 
 
 
